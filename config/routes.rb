4e25b9ae56d6fc95333d1b6ed705826a4aeeac20
Ania Czekaj <Akczekaj@gmail.com>
2018-09-05 12:26:50 +0800

    menu done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get '/menu', to: 'pages#menu'
+  get '/ta_menu', to: 'pages#ta_menu'
   get '/our_story', to: 'pages#our_story'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
